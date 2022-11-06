a8a49c9d169de43266651f31f67532e15b95cc20
SyedHurairaRaza <syedhurairaraza.114@gmail.com>
2022-11-07 02:13:06 +0500

    work done

---
+++ public/js app.js
@@ -0,0 +1,62 @@
+// var eng = +prompt("enter eng marks");
+// var ai = +prompt("enter ai marks");
+// var maths = +prompt("enter maths marks");
+// var isl = +prompt("enter isl marks");
+// var engg = +prompt("enter engg marks");
+// console.log(eng);
+// console.log(ai);
+// console.log(maths);
+// console.log(isl);
+// console.log(engg);
+// var total=parseFloat(eng)+parseFloat(ai)+parseFloat(maths)+parseFloat(isl)+parseFloat(engg);
+// var per = (total/500*100);
+// console.log(per)
+
+
+// if(per >= 60 && per <= 70 ){
+//     console.log("c grade")
+// }
+// else if(per >= 70 && per <= 80 ){
+//     console.log("b grade")
+// }
+// else if(per >= 80 && per<=90 ){
+//     console.log("a grade")
+// }
+// else
+// {
+//     console.log("fail")
+// }
+
+
+
+
+// var bitch = "sucker";
+
+// if  (bitch == "sucker"){
+//     console.log("sucker certified");
+// }else{
+//     console.log("not a sucker");
+// }
+
+// var age = prompt("enter your age");
+// var license = false;
+
+// if (age > 18){
+//     console.log("drive the car")
+// }
+// else if(license == true){
+//     console.log("you can  drive the car ")
+// } else{
+//     console.log("you not")
+// }
+
+
+var num = prompt("enter num");
+if(num != 10){
+    if(num>5){
+console.log("number is great than 5")
+    }
+    else{
+        console.log("not eaqual to 10")
+    }
+}
